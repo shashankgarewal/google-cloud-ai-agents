f0538baa6196cf3f30f726087469572877bc8649
Shashank Garewal <shashankgarewal4+github@gmail.com>
2026-03-19 17:50:19 +0000

    Add logging to setup agent

---
+++ agent.py
@@ -0,0 +1,25 @@
+import os
+import logging
+import google.cloud.logging
+from dotenv import load_dotenv
+
+from google.adk import Agent
+from google.adk.agents import SequentialAgent
+from google.adk.tools.tool_context import ToolContext
+from google.adk.tools.langchain_tool import LangchainTool
+
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+
+import google.auth
+import google.auth.transport.requests
+import google.oauth2.id_token
+
+# --- Setup Logging and Environment ---
+
+cloud_logging_client = google.cloud.logging.Client()
+cloud_logging_client.setup_logging()
+
+load_dotenv()
+
+model_name = os.getenv("MODEL")
