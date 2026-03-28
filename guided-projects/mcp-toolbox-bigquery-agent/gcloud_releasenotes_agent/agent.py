005e09c4abb385c248f2cbeb673177de66fad427
shashankgarewal <shashankgarewal4+github@gmail.com>
2026-03-28 22:25:47 +0530

    feat(agent): connect root_agent to MCP Toolbox client (default instruction)

--- guided-projects/mcp-toolbox-bigquery-agent/gcloud_releasenotes_agent/agent.py
+++ guided-projects/mcp-toolbox-bigquery-agent/gcloud_releasenotes_agent/agent.py
@@ -1,8 +1,13 @@
 from google.adk.agents.llm_agent import Agent
+from toolbox_core import ToolboxSyncClient
+
+bq_toolbox  = ToolboxSyncClient("http://127.0.0.1:5000")
+bq_tools    = bq_toolbox.load_toolset('my_bq_toolset')
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
+    tools=bq_tools,
 )
