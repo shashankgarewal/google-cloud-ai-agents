6ff60457341b75f9fcc40edf1517e86d401e36d5
shashankgarewal <shashankgarewal4+github@gmail.com>
2026-03-28 23:13:21 +0530

    feat(agent): customize root_agent instruction for BigQuery queries

--- guided-projects/mcp-toolbox-bigquery-agent/gcloud_releasenotes_agent/agent.py
+++ guided-projects/mcp-toolbox-bigquery-agent/gcloud_releasenotes_agent/agent.py
@@ -7,7 +7,10 @@ bq_tools    = bq_toolbox.load_toolset('my_bq_toolset')
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description="Agent to answer questions about Google Cloud Release notes.",
+    instruction="""
+        You are a helpful agent with acces to bq_tools. 
+        Your goal is to fully utilize the bq_tools and the answer user's question about Google Cloud Release notes
+        """,
     tools=bq_tools,
 )
