d8fcae5ebf956dd8fa5be62e82c90f00ce5b1016
shashankgarewal <shashankgarewal4+github@gmail.com>
2026-03-31 15:47:40 +0530

    feat: fetch data in small batches

--- self-projects/crop-best-pricing-tools/mandi_agent/agent.py
+++ self-projects/crop-best-pricing-tools/mandi_agent/agent.py
@@ -1,6 +1,6 @@
 from google.adk.agents.llm_agent import Agent
 from google.adk.tools import FunctionTool
-from tools.fetch_price import get_mandi_prices
+from tools.fetch_price import get_current_mandi_prices
 from tools.transport_cost import estimate_transport_cost
 
 root_agent = Agent(
@@ -10,15 +10,17 @@ root_agent = Agent(
     instruction="""
     Your job:
     - Help farmers find the best market (mandi) to sell crops
-    - Always use the get_mandi_prices tool when crop price is needed
+    - Always use the get_current_mandi_prices tool when crop price is needed
     - In case farmer need general farming guidance, respond with best of your capabilities and also mention your primary function
     Communication Strategy:
     1. USER-CENTRIC TONE: Match the user's level of technicality. If the user uses slang, respond with helpful, grounded "Mandi" lingo.
     2. AREA-BASED ADAPTATION: Automatically adjust terminology and language based on the 'State' and 'District' data. 
     - Example: Use 'Kanda' in Maharashtra/MH and 'Pyaj' in MP/North India. Focus on regional units (e.g., Quintals).
     3. ALPHABET MIRRORING: Follow the user scripting and lingo.
-    - If the user writes in the English alphabet (e.g., "bhav kya hai"), you MUST respond in the English alphabet (Hinglish). Do not use Devanagari/Hindi script.
+    - Example: If the user writes in the Hinglish (e.g., "bhav kya hai"), you MUST respond in the Hinglish (i.e, Hindi and English but writen with English alphabet). Do not use pure Devanagari/Hindi script.
     """,
-    tools=[FunctionTool(get_mandi_prices), 
+    tools=[FunctionTool(get_current_mandi_prices), 
            FunctionTool(estimate_transport_cost)],
 )
+
+
--- self-projects/crop-best-pricing-tools/tools/fetch_price.py
+++ self-projects/crop-best-pricing-tools/tools/fetch_price.py
@@ -1,13 +1,22 @@
 import os
+import math
 import requests
 from dotenv import load_dotenv
-from pydantic import BaseModel, Field
-from typing import List
+from pydantic import BaseModel, Field, field_validator
+from datetime import date, datetime
+from typing import List, Optional
+from itertools import product
 
 load_dotenv("../.env")
 api_key = os.getenv("MANDI_DATA_API_KEY")
+
 primary_url = "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070"
 fallback_url = "https://api.data.gov.in/resource/35985678-0d79-46b4-9ed6-6f13308a1d24"
+
+# guardrail - prevent from extreme api calls 
+BATCH_SIZE = 100
+MAX_PAGES = 10
+MAX_COMBINATIONS = 10
 
 # ------------------------------- output schema ------------------------------ #
 class MandiRecord(BaseModel):
@@ -16,24 +25,37 @@ class MandiRecord(BaseModel):
     market: str = Field(alias="market")
     commodity: str = Field(alias="commodity")
     variety: str = Field(alias="variety")
-    arrival_date: str = Field(alias="arrival_date")
+    arrival_date: date = Field(alias="arrival_date")
     min_price: float = Field(alias="min_price")
     max_price: float = Field(alias="max_price")
     modal_price: float = Field(alias="modal_price")
+    
+    @field_validator("arrival_date", mode="before")
+    @classmethod
+    def parse_date(cls, raw_date):
+        if isinstance(raw_date, str):
+            return datetime.strptime(raw_date, "%d/%m/%Y").date()
+        return raw_date
 
 class MandiResponse(BaseModel):
     records: List[MandiRecord]
+    total_fetched: int
+    total_records: int
 
 class ErrorResponse(BaseModel):
     error: str
     
 # ---------------------------------- helper ---------------------------------- #
 
-def _build_params(crop: str, state: str, district: str, mandi: str, extra_filters: dict) -> dict:
+def _build_params(crop, state, district, mandi, 
+                  offset=0, limit=BATCH_SIZE):
 
     params = {
         "api-key": api_key,
         "format": "json",
+        "limit": limit,
+        "offset": offset,
+
     }
     
     if crop:
@@ -45,15 +67,17 @@ def _build_params(crop: str, state: str, district: str, mandi: str, extra_filter
     if mandi:
         params["filters[market]"] = mandi
 
-    if extra_filters:
-        params.update(extra_filters)
-
     return params
 
+
 # ------------------------------- main function ------------------------------ #
-def get_mandi_prices(crop: str, state: str = None, district: str = None, mandi: str = None):
+def get_current_mandi_prices(crops: Optional[List[str]] = None,
+                             states: Optional[List[str]] = None,
+                             districts: Optional[List[str]] = None,
+                             mandis: Optional[List[str]] = None
+                             ):
     """
-    Fetches latest prices of a crop at different mandis (markets) from India's Agmarknet database.
+    Fetches latest price data with support for multiple filters
 
     Args:
         crop (str): crop name e.g., "onion", "wheat" in api supported format
@@ -61,22 +85,81 @@ def get_mandi_prices(crop: str, state: str = None, district: str = None, mandi: 
         district: Optional district name, e.g. "Dewas", "Indore", "Nashik".
         mandi: Optional specific market/mandi name, e.g. "Dewas", "Azadpur".
     """
-    primary_params = _build_params(crop, state, district, mandi, {})
+    crop_list = crops or [None]
+    state_list = states or [None]
+    district_list = districts or [None]
+    mandi_list = mandis or [None]
     
-    try:
-        response = requests.get(primary_url, params=primary_params)
-        
-        if response.status_code != 200:
-            return ErrorResponse(error="API request failed")
-        
-        data = response.json()
-        raw_records = data.get("records", [])
-        
-        if not raw_records:
-            return ErrorResponse(error="No data found for given filters today")
-        
-        records = [MandiRecord(**r) for r in raw_records]
-    except Exception as e:
-        return ErrorResponse(error=str(e))
+    # Guardrail
+    total_combinations = (len(crop_list) * 
+                          len(state_list) *
+                          len(district_list) * 
+                          len(mandi_list)
+                          )
+    
+    if total_combinations > MAX_COMBINATIONS:
+        return ErrorResponse(error="Too many filter combinations requested")
 
-    return MandiResponse(records=records)
+    fetched_records = []    # combine records list fetched for filter combination
+    available_records = 0     # total records in api database - may not fetch all record 
+    
+    # --------------------- Iterate over filter combinations --------------------- #
+    for crop, state, district, mandi in product(
+        crop_list, state_list, district_list, mandi_list
+        ):
+        # ---------------------------- probe total record ---------------------------- #
+        try:
+            probe_params = _build_params(
+                crop, state, district, mandi, offset=0, limit=1
+                )
+            response = requests.get(primary_url, params=probe_params, timeout=8)
+            if response.status_code != 200 or not response.content:
+                continue
+            
+            data = response.json()
+            total = int(data.get("total", 0))
+            available_records += total
+            
+        except Exception:
+            continue
+        
+        # skip this filters combo - no record
+        if total == 0:
+            continue
+                    
+        # -------------------------- fetch record in batches ------------------------- #
+        
+        # guardrail - prevent too many api requests
+        pages = math.ceil(total / BATCH_SIZE)
+        pages = min(pages, MAX_PAGES)
+        
+        for page in range(pages):
+            offset = page * BATCH_SIZE
+                
+            primary_params = _build_params(
+                crop, state, district, mandi, offset=offset
+                )
+            
+            try:
+                response = requests.get(primary_url, params=primary_params, timeout=8)
+                
+                if response.status_code != 200 or not response.content:
+                    break
+                
+                data = response.json()
+                raw_records = data.get("records", [])
+                
+                if not raw_records:
+                    break
+                
+                fetched_records.extend(
+                    [MandiRecord(**r) for r in raw_records]
+                    )
+            except Exception:
+                continue
+            
+
+    return MandiResponse(records=fetched_records,
+                         total_fetched = len(fetched_records),
+                         total_records = available_records
+                         )
